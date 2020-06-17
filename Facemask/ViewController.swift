fc9a56f8742a02318e6a17169b5a2248f1b275fd
RyotaNISHIOKA <mlf.r24oka@gmail.com>
2020-06-17 13:33:41 +0900

    initial commit

--- Facemask/ViewController.swift
+++ Facemask/ViewController.swift
@@ -14,31 +14,58 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
+        UIApplication.shared.isIdleTimerDisabled = false
+        
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        //sceneView.showsStatistics = true // ダサいので無効化
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!//無効化
         
         // Set the scene to the view
-        sceneView.scene = scene
+        //sceneView.scene = scene//無効化
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
 
-        // Run the view's session
-        sceneView.session.run(configuration)
+//        // Create a session configuration
+//        let configuration = ARWorldTrackingConfiguration()
+//        configuration.environmentTexturing = .automatic
+//            configuration.frameSemantics = .personSegmentationWithDepth
+//
+//        // Run the view's session
+//        sceneView.session.run(configuration)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        resetTracking()
+    }
+    
+    private func resetTracking(){
+        guard ARFaceTrackingConfiguration.isSupported else{
+            return
+        }
+        let configuration = ARFaceTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true
+
+      
+        //let options = [.resetTracking, .removeExistingAnchors]//letへの代入がうまく行かないのでコメントアウト
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])//直接入力
+
+    }
+    
+
+    
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
@@ -49,14 +76,38 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
+    private let shipNode: SCNReferenceNode? = {
+        let path = Bundle.main.path(forResource: "face",
+            ofType: "scn",
+            inDirectory: "art.scnassets")!
+        let url = URL(fileURLWithPath: path)
+        return SCNReferenceNode(url: url)
+        
+      
+    }()
+    
+    
+
     // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor){
+        guard anchor is ARFaceAnchor else{
+    return
     }
-*/
+        
+        if node.childNodes.isEmpty, let content = shipNode{
+            content.load()
+            
+            let constraint = SCNBillboardConstraint()
+            constraint.freeAxes = [.X, .Y]
+            content.constraints = [constraint]
+            
+
+            
+            node.addChildNode(content)
+        }
+    
+    
+
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -72,4 +123,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+}
 }
